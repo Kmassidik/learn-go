c5957407f5b3f3a6d019779179fb41ce0f53973a
Kurnia Massidik <k.massidik@gmail.com>
2025-01-08 17:32:56 +0700

    feat: add env to port used, finish task web-server

--- beginner-level/web-server/main.go
+++ beginner-level/web-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -13,15 +14,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+func notFoundHandler(w http.ResponseWriter, _ *http.Request) {
 	http.Error(w, "404 Not Found", http.StatusNotFound)
 }
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/", handler)
 
-	fmt.Println("Starting server at :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Printf("Starting server at :%s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Println("Server failed:", err)
 	}
 }
